0210f91aacaacb5b50b8225aba4b5d999cc221ac
Brown-scripts <nikkibrowm297@gmail.com>
2025-01-05 17:04:09 +0000

    added logs

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const app = express();
 app.use(cors());
 const proxy = httpProxy.createProxyServer();
 
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
 const PORT = process.env.PORT || 3000;
 
 // Services URLs
