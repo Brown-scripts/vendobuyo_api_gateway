cf07635d29c1ac583f178e4b98aa018a1fe37b97
Brown-scripts <nikkibrowm297@gmail.com>
2025-02-01 21:20:40 +0000

    added admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,3 +13,23 @@ exports.authenticateToken = (req, res, next) => {
   });
 };
 
+exports.authenticateAdmin = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.role === "admin") {
+      req.user = decoded;
+    } else {
+      return res.status(403).json({ message: 'Admin required' });
+    }
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
